31a31a9368c3f30d1f9ebadb04dd57de36f277a1
zzzzgc <1098766713@qq.com>
2018-01-22 11:38:29 +0800

    暂时注释微信支付

--- src/components/shopping/OrderInfo.vue
+++ src/components/shopping/OrderInfo.vue
@@ -103,7 +103,6 @@
   import BottomMenu from '../common/BottomMenu'
   import OrderInfoList from './OrderInfoList'
   import TitleInfo from '../common/TitleInfo'
-  import MD5 from '../../common/md5'
 
   export default {
     name: 'orderInfo',
@@ -170,7 +169,7 @@
           nonceStr: '',
           prepay_id: '',
           paySign: '',
-          signType: 'MD5'
+          signType: ''
         }
 
       }
@@ -193,31 +192,31 @@
         console.log('payInfo:')
         console.log(this.payInfo)
         // TODO 微信拉起支付
-        if (typeof WeixinJSBridge == 'undefined') {
-          if (document.addEventListener) {
-            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-          } else if (document.attachEvent) {
-            document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-          }
-        } else {
-          oWeixinJSBridge.invoke(
-            'getBrandWCPayRequest', {
-              "appId": this.payInfo.appId,     // TODO 公众号名称，由商户传入
-              "timeStamp": this.payInfo.timeStamp,         //时间戳，自1970年以来的秒数
-              "nonceStr": this.payInfo.nonceStr, //随机串
-              "package": "prepay_id=" + this.payInfo.prepay_id,
-              "signType": this.payInfo.signType,         //微信签名方式：
-              "paySign": this.payInfo.paySign //微信签名
-            },
-            function (res) {
-              if ('get_brand_wcpay_request:ok' == res.err_msg) {
-                // TODO 跳转到成功页面 ,需要添加页面
-                this.$router.push('')
-              }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-            }
-          );
-        }
+        // if (typeof WeixinJSBridge == 'undefined') {
+        //   if (document.addEventListener) {
+        //     document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false)
+        //   } else if (document.attachEvent) {
+        //     document.attachEvent('WeixinJSBridgeReady', onBridgeReady)
+        //     document.attachEvent('onWeixinJSBridgeReady', onBridgeReady)
+        //   }
+        // } else {
+        //   oWeixinJSBridge.invoke(
+        //     'getBrandWCPayRequest', {
+        //       'appId': this.payInfo.appId,     // TODO 公众号名称，由商户传入
+        //       'timeStamp': this.payInfo.timeStamp,         // 时间戳，自1970年以来的秒数
+        //       'nonceStr': this.payInfo.nonceStr, // 随机串
+        //       'package': 'prepay_id=' + this.payInfo.prepay_id,
+        //       'signType': this.payInfo.signType,         // 微信签名方式：
+        //       'paySign': this.payInfo.paySign // 微信签名
+        //     },
+        //     function (res) {
+        //       if (res.data.err_msg === 'get_brand_wcpay_request:ok') {
+        //         // TODO 跳转到成功页面 ,需要添加页面
+        //         this.$router.push('')
+        //       }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+        //     }
+        //   )
+        // }
       },
       getProducts: function () {
         // 获取下单的商品信息
