ae2fb825ecbd456637e4258f53e1e87f8e5da847
zzzzgc <1098766713@qq.com>
2018-01-22 11:15:48 +0800

    添加 商户认证表和商户信息表

--- src/components/shopping/OrderInfo.vue
+++ src/components/shopping/OrderInfo.vue
@@ -2,10 +2,10 @@
   <div class="order-info">
     <!--TopBar-->
     <!--<div class="top-fixed-header header">-->
-      <!--<div class="back" @click="$router.go(-1)">-->
-        <!--<icon scale="1.6" name="angle-left"></icon>-->
-      <!--</div>-->
-      <!--<div class="title">获取的数据</div>-->
+    <!--<div class="back" @click="$router.go(-1)">-->
+    <!--<icon scale="1.6" name="angle-left"></icon>-->
+    <!--</div>-->
+    <!--<div class="title">获取的数据</div>-->
     <!--</div>-->
     <TitleInfo titleContent="填写订单"></TitleInfo>
 
@@ -35,7 +35,7 @@
       <div class="products">
         <div v-for="(product, index) in products" :key="product.id">
           <div class="product">
-            <img  :src='product.img' alt="">
+            <img :src='product.img' alt="">
             <span class="top_left_str">[{{product.country =='中国'?'国产':'进口'}}]{{product.product_name}}</span>
             <span class="sub_str">{{product.product_standard_name}}</span>
             <span class="lower_right_str"><span
@@ -69,9 +69,11 @@
     width: 100%;
     height: 44px;
     background: white;">
-      <tr >
+      <tr>
         <td style="z-index: 2;text-align: right;color: red;">实付款: ￥{{this.countPrice}}</td>
-        <td style="z-index: 2;width: 40%;background-color: red;text-align: center;color: #fff;"><mt-button name="submit" type='danger' @click="getPay" size='large'>提交订单</mt-button></td>
+        <td style="z-index: 2;width: 40%;background-color: red;text-align: center;color: #fff;">
+          <mt-button name="submit" type='danger' @click="getPay" size='large'>提交订单</mt-button>
+        </td>
       </tr>
     </table>
     <!--<mt-tabbar  v-model="selected">-->
@@ -102,6 +104,7 @@
   import OrderInfoList from './OrderInfoList'
   import TitleInfo from '../common/TitleInfo'
   import MD5 from '../../common/md5'
+
   export default {
     name: 'orderInfo',
     computed: {
@@ -190,25 +193,25 @@
         console.log('payInfo:')
         console.log(this.payInfo)
         // TODO 微信拉起支付
-        if (typeof WeixinJSBridge == 'undefined'){
-          if( document.addEventListener ){
+        if (typeof WeixinJSBridge == 'undefined') {
+          if (document.addEventListener) {
             document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-          }else if (document.attachEvent){
+          } else if (document.attachEvent) {
             document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
             document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
           }
-        }else{
+        } else {
           oWeixinJSBridge.invoke(
             'getBrandWCPayRequest', {
-              "appId":this.payInfo.appId,     // TODO 公众号名称，由商户传入
-              "timeStamp":this.payInfo.timeStamp,         //时间戳，自1970年以来的秒数
-              "nonceStr":this.payInfo.nonceStr, //随机串
-              "package":"prepay_id=" + this.payInfo.prepay_id,
-              "signType":this.payInfo.signType,         //微信签名方式：
-              "paySign":this.payInfo.paySign //微信签名
+              "appId": this.payInfo.appId,     // TODO 公众号名称，由商户传入
+              "timeStamp": this.payInfo.timeStamp,         //时间戳，自1970年以来的秒数
+              "nonceStr": this.payInfo.nonceStr, //随机串
+              "package": "prepay_id=" + this.payInfo.prepay_id,
+              "signType": this.payInfo.signType,         //微信签名方式：
+              "paySign": this.payInfo.paySign //微信签名
             },
-            function(res){
-              if('get_brand_wcpay_request:ok' == res.err_msg ) {
+            function (res) {
+              if ('get_brand_wcpay_request:ok' == res.err_msg) {
                 // TODO 跳转到成功页面 ,需要添加页面
                 this.$router.push('')
               }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
