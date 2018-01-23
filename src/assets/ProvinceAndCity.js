986b845a26f02fde90c1cea2be2975c4404ead27
partnerccz <2779017796@qq.com>
2018-01-23 11:07:31 +0800

    添加商品信息（二级联动）等

---
+++ src/assets/ProvinceAndCity.js
@@ -0,0 +1,48 @@
+export const address = [
+  {
+    'code': '0001',
+    'showName': '广东省',
+    'city': [
+      {
+        'code': '0001-1',
+        'showName': '深圳市'
+      }, {
+        'code': '0001-2',
+        'showName': '广州市'
+      }, {
+        'code': '0001-3',
+        'showName': '东莞市'
+      }
+    ]
+  }, {
+    'code': '0002',
+    'showName': '福建省',
+    'city': [
+      {
+        'code': '0002-1',
+        'showName': '漳州市'
+      }, {
+        'code': '0002-2',
+        'showName': '莆田市'
+      }, {
+        'code': '0002-3',
+        'showName': '福州市'
+      }
+    ]
+  }, {
+    'code': '0003',
+    'showName': '湖南省',
+    'city': [
+      {
+        'code': '0003-1',
+        'showName': '长沙市'
+      }, {
+        'code': '0003-2',
+        'showName': '株洲市'
+      }, {
+        'code': '0003-3',
+        'showName': '常德市'
+      }
+    ]
+  }
+]
--- src/components/person/ShopInfo.vue
+++ src/components/person/ShopInfo.vue
@@ -2,65 +2,181 @@
   <div class="shop-info-container">
     <title-info titleContent="店铺信息"></title-info>
     <div class="shop-detail">
-      <mt-field label="店铺名称:" style="color:#ff0000;" placeholder="所在城市+店名，如广州嘻果"></mt-field>
-      <span style="display: block;color:#D6CCD6;width:100%;margin:-5px 0 0 10px;">店铺名称仅限中文+数字，不超过10个字符</span>
+      <mt-field label="店铺名称:" placeholder="所在城市+店名，如广州嘻果" v-model="businessInfo.business_name"></mt-field>
+      <span style="display: block;color:#D6CCD6;width:100%;text-indent: 10px;">店铺名称仅限中文+数字，不超过10个字符</span>
       <!--<mt-cell style="color:#D6CCD6;" title="店铺名称仅限中文+数字，不超过10个字符"></mt-cell>-->
-      <mt-field  label="联系人：" placeholder="请输入联系人"></mt-field>
-      <mt-field  label="联系电话：" placeholder="请输入联系电话"></mt-field>
+      <mt-field  label="联系人："  placeholder="请输入联系人" v-model="businessInfo.business_contacts"></mt-field>
+      <mt-field  label="联系电话：" placeholder="请输入联系电话" v-model="businessInfo.phone"></mt-field>
       <div style="display: flex;flex-direction: row;background-color: white;">
-        <mt-field  label="省     市：" style="flex:1;">
-        </mt-field>
-        <div style="flex: 1;line-height: 44px;">
-          <select style="width:50%;text-align: left;">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-          </select>
-        </div>
-        <div style="flex: 1;line-height: 44px;">
-          <select style="width:50%;text-align: left;">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-          </select>
-        </div>
+        <mt-cell style="flex:0.5;color: #ff0000;"  title="省      市：">
+        </mt-cell>
+        <div style="flex:1;margin-top:8px;text-align: left;color:#000;" @click.navite="showProvinceAndCity">{{businessInfo.address_province}}-{{businessInfo.address_city}}</div>
       </div>
-      <mt-field  label="收货地址：" placeholder="请输入收货地址"></mt-field>
-      <mt-field  label="店铺地址：" placeholder="若与收货地址一致，无需填写"></mt-field>
+      <mt-field  label="收货地址：" placeholder="请输入收货地址" v-model="businessInfo.address_detail"></mt-field>
+      <mt-field  label="店铺地址：" placeholder="若与收货地址一致，无需填写" v-model="businessInfo.address_shop"></mt-field>
       <div style="display: flex;flex-direction: row;background-color: white;">
         <mt-cell style="color:#ff0000;flex: 0.5;" title="物流方式："></mt-cell>
-        <div style="flex: 1;line-height: 44px;">
-          <select style="width:25%;text-align: left;">
-            <option>请选择</option>
-            <option>市场车</option>
-            <option>物流</option>
-            <option>自提</option>
-          </select>
+        <div style="flex: 1;line-height: 44px;margin-top:4px;text-align: left;color:#000;">
+          <div @click.navite="showShipmentsType">{{businessInfo.shipments_type}}</div>
         </div>
       </div>
-      <mt-button size="large" type="primary" style="margin-top:20px;">确认</mt-button>
+      <mt-popup position="bottom" v-model="isShowShipmentsType" style="width:100%;padding: 10px 0;">
+        <mt-picker :slots="shipmentsType" :visible-item-count="5" @change="onValuesChange"></mt-picker>
+      </mt-popup>
+      <mt-popup position="bottom" v-model="isShowProvinceAndCity" style="width:100%;padding:20px 0;">
+        <mt-picker :slots="addressProvince" value-key="showName" :visible-item-count="5" @change="onValuesChange2"></mt-picker>
+      </mt-popup>
+      <mt-button size="large" type="primary" style="margin-top:20px;" @click.navite="addBusinessInfo">确认</mt-button>
     </div>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
   import TitleInfo from '../common/TitleInfo'
-  import MtField from 'mint-ui/packages/field/src/field'
+  import Field from 'mint-ui/packages/field/src/field'
   import 'mint-ui/lib/style.css'
-  import MtCell from 'mint-ui/packages/cell/src/cell'
-  import MtButton from 'mint-ui/packages/button/src/button'
+  import Cell from 'mint-ui/packages/cell/src/cell'
+  import Button from 'mint-ui/packages/button/src/button'
+  import Mint from 'mint-ui'
+  import Picker from 'mint-ui/packages/picker/src/picker'
+  import Popup from 'mint-ui/packages/popup/src/popup'
+  import {address} from '../../assets/ProvinceAndCity'
+  import {validatePhone} from '../../common/FormValidate'
 
+  Vue.use(Mint)
   export default {
     components: {
-      MtButton,
-      MtCell,
-      MtField,
+      Popup,
+      Picker,
+      Button,
+      Cell,
+      Field,
       TitleInfo},
     name: 'shop-info',
-    component: {
-      TitleInfo
+    data: function () {
+      return {
+        businessInfo: {
+          business_name: '',
+          business_contacts: '',
+          phone: '',
+          address_province: '请选择',
+          address_city: '请选择',
+          address_detail: '',
+          address_shop: '',
+          shipments_type: '请选择'
+        },
+        shipmentsType: [
+          {values: ['请选择', '市场车', '物流', '自提']}
+        ],
+        isShowShipmentsType: false,
+        addressProvince: [
+          {
+            flex: 1,
+            values: address,
+            defaultIndex: 10,
+            className: 'slot1',
+            textAlign: 'center'
+          }, {
+            divider: true,
+            content: '-',
+            defaultIndex: 0,
+            className: 'slot2'
+          }, {
+            flex: 1,
+            values: address[0].city,
+            defaultIndex: 0,
+            className: 'slot3',
+            textAlign: 'center'
+          }
+        ],
+        isShowProvinceAndCity: false
+      }
+    },
+    methods: {
+      addBusinessInfoBefore: function () {
+        var flag = true
+        var showError = ''
+        if (this.businessInfo.business_name.trim() === '') {
+          flag = false
+          showError = '商户名称'
+        } else if (this.businessInfo.business_contacts.trim() === '') {
+          flag = false
+          showError = '联系人'
+        } else if (!validatePhone(this.businessInfo.phone)) {
+          flag = false
+          showError = '电话号码'
+        } else if (this.businessInfo.address_province.trim() === '' || this.businessInfo.address_province.trim() === '请选择') {
+          flag = false
+          showError = '省份'
+        } else if (this.businessInfo.address_city.trim() === '' || this.businessInfo.address_city.trim() === '请选择') {
+          flag = false
+          showError = '城市'
+        } else if (this.businessInfo.address_detail.trim() === '') {
+          flag = false
+          showError = '收货地址'
+        } else if (this.businessInfo.shipments_type.trim() === '' || this.businessInfo.shipments_type.trim() === '请选择') {
+          flag = false
+          showError = '物流方式'
+        }
+        if (!flag) {
+          this.$toast(showError + '输入格式有误')
+        } else {
+          if (this.businessInfo.address_shop.trim() === '') {
+            this.businessInfo.address_shop = this.businessInfo.address_detail
+          }
+        }
+        return flag
+      },
+      addBusinessInfo: function () {
+        if (this.addBusinessInfoBefore()) {
+          this.$http.post('/businessInfo/addBusinessInfo', this.businessInfo).then((response) => {
+            if (response.data === 1) {
+              this.$toast('添加成功')
+              this.businessInfo.business_name = ''
+              this.businessInfo.business_contacts = ''
+              this.businessInfo.phone = ''
+              this.businessInfo.address_province = '请选择'
+              this.businessInfo.address_city = '请选择'
+              this.businessInfo.address_detail = ''
+              this.businessInfo.address_shop = ''
+              this.businessInfo.shipments_type = '请选择'
+            }
+          })
+        }
+      },
+      showShipmentsType: function () {
+        if (!this.isShowShipmentsType) {
+          this.isShowShipmentsType = true
+        }
+      },
+      onValuesChange (picker, values) {
+        this.businessInfo.shipments_type = values[0]
+        if (values[0] > values[1]) {
+          picker.setSlotValue(1, values[0])
+        }
+      },
+      onValuesChange2 (picker, values) {
+        if (!values[0]) {
+          this.$nextTick(() => {
+            picker.setValues([address[0], address[0].city[0]])
+          })
+        } else {
+          picker.setSlotValues(1, values[0].city)
+          let town = []
+          if (values[1]) {
+            town = values[1].city
+          }
+          picker.setSlotValues(2, town)
+        }
+        if (typeof (values) !== 'undefined' && typeof (values[0]) !== 'undefined' && typeof (values[1]) !== 'undefined' && typeof (values[0].showName) !== 'undefined' && typeof (values[1].showName) !== 'undefined') {
+          this.businessInfo.address_province = values[0].showName
+          this.businessInfo.address_city = values[1].showName
+        }
+      },
+      showProvinceAndCity: function () {
+        this.isShowProvinceAndCity = true
+      }
     }
   }
 </script>
