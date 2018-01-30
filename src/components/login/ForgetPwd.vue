bf279a22b13cfbfc36468c4ae4769dd75bfb8a47
partnerccz <2779017796@qq.com>
2018-01-30 13:00:31 +0800

    忘记密码的表单提交验证

--- src/components/login/ForgetPwd.vue
+++ src/components/login/ForgetPwd.vue
@@ -7,12 +7,12 @@
         <div class="title-desc">忘记密码</div>
       </div>
       <div class="content">
-        <mt-field label="手机号码"  placeholder="请输入手机号码" style="color:red;" class="horizontal"></mt-field>
-        <mt-field label="验 证 码"  placeholder="请输入验证码" style="color:red;" class="horizontal">
+        <mt-field label="手机号码"  v-model="phone" placeholder="请输入手机号码" :attr="{ maxlength:11}" style="color:red;" class="horizontal"></mt-field>
+        <mt-field label="验 证 码" placeholder="请输入验证码" style="color:red;" class="horizontal">
           <mt-cell title="获取验证码" to="www.baidu.com" class="authcode"></mt-cell>
         </mt-field>
-        <mt-field label="新 密 码" type="password"  placeholder="请输入最少6位数密码" style="color:red;" class="horizontal"></mt-field>
-        <mt-button type="primary" size="large" class="registerButton">重设密码</mt-button>
+        <mt-field label="新 密 码" v-model="password" type="password"  placeholder="请输入最少6位数密码" :attr="{maxlength: 18 }"  style="color:red;" class="horizontal"></mt-field>
+        <mt-button type="primary" size="large" class="registerButton" @click.navite="resetPwdForForget">重设密码</mt-button>
       </div>
     </div>
 </template>
@@ -23,12 +23,42 @@
   import Icon from 'vue-awesome/components/Icon'
   import Mint from 'mint-ui'
   import 'mint-ui/lib/style.css'
+  import {validatePhone, validateStrLength} from '../../common/FormValidate'
 
   Vue.use(Mint)
   export default {
     name: 'forget-pwd',
     components: {
       Icon
+    },
+    data: function () {
+      return {
+        phone: '',
+        validCode: '',
+        password: ''
+      }
+    },
+    methods: {
+      resetPwdForForgetBefore: function () {  // 重设密码的提交表单的校验
+        var flag = true
+        var errorMsg = ''
+        if (!validatePhone(this.phone)) {
+          flag = false
+          errorMsg = '手机号码'
+        } else if (!validateStrLength(this.password)) {
+          flag = false
+          errorMsg = '新密码'
+        }
+        if (!flag) {
+          this.$toast(errorMsg + '输入的格式有误!')
+        }
+        return flag
+      },
+      resetPwdForForget: function () {
+        if (!this.resetPwdForForgetBefore()) {
+          return false
+        }
+      }
     }
   }
 </script>
