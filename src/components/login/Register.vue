d9116728b77ccf3e32ebca87f90484d0290fc684
partnerccz <2779017796@qq.com>
2018-01-30 13:20:50 +0800

    注册页面的表单提交验证

--- src/components/login/Register.vue
+++ src/components/login/Register.vue
@@ -7,17 +7,17 @@
       <div class="title-desc">快速注册</div>
     </div>
     <div class="content">
-      <mt-field label="手机号码"  placeholder="请输入手机号码" style="color:red;" class="horizontal"></mt-field>
+      <mt-field label="手机号码" v-model="phone" :attr="{ maxlength:11}"  placeholder="请输入手机号码" style="color:red;" class="horizontal"></mt-field>
       <mt-field label="验 证 码"  placeholder="请输入验证码" style="color:red;" class="horizontal">
         <mt-cell title="获取验证码" to="www.baidu.com" class="authcode"></mt-cell>
       </mt-field>
-      <mt-field label="密   码" type="password"  placeholder="请输入最少6位数密码" style="color:red;" class="horizontal"></mt-field>
+      <mt-field label="密   码" v-model="password" :attr="{maxlength: 18 }" type="password"  placeholder="请输入最少6位数密码" style="color:red;" class="horizontal"></mt-field>
       <mt-field label="邀 请 码"  placeholder="留空则由系统为您分配业务员" class="horizontal"></mt-field>
-      <mt-button type="primary" size="large" class="registerButton">注册</mt-button>
+      <mt-button type="primary" size="large" class="registerButton" @click.navite="handleRegister">注册</mt-button>
       <mt-cell ><span class="serviceTip">点击注册，即代表同意</span>
         <div class="serviceProtocol" @click.navite="showServiceProtocol">《**服务协议》</div>
       </mt-cell>
-      <div class="afterSaleByPhone">遇到问题，请致电 : 02009090999</div>
+      <div class="afterSaleByPhone">遇到问题，请致电 : 020-81636906</div>
     </div>
   </div>
 </template>
@@ -28,15 +28,42 @@
   import Icon from 'vue-awesome/components/Icon'
   import Mint from 'mint-ui'
   import 'mint-ui/lib/style.css'
+  import {validatePhone, validateStrLength} from '../../common/FormValidate'
   Vue.use(Mint)
   export default {
     components: {
       Icon
     },
     name: 'register',
+    data: function () {
+      return {
+        phone: '',
+        password: ''
+      }
+    },
     methods: {
       showServiceProtocol: function () {
         window.location.href = './serviceProtocol'
+      },
+      handleRegisterBefore: function () { // 注册表单提交校验
+        var flag = true
+        var errorMsg = ''
+        if (!validatePhone(this.phone)) {
+          flag = false
+          errorMsg = '手机号码'
+        } else if (!validateStrLength(this.password)) {
+          flag = false
+          errorMsg = '密码'
+        }
+        if (!flag) {
+          this.$toast(errorMsg + '输入格式有误!')
+        }
+        return flag
+      },
+      handleRegister: function () {
+        if (!this.handleRegisterBefore()) {
+          return false
+        }
       }
     }
   }
