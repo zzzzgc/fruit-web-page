672dff41a03dd62f15782b878e9dd199068715b2
partnerccz <2779017796@qq.com>
2018-02-06 17:08:12 +0800

    添加修改密码提交之前的表单验证

--- src/components/login/UpdatePwd.vue
+++ src/components/login/UpdatePwd.vue
@@ -2,17 +2,17 @@
   <div class="update-pwd">
     <title-info titleContent="修改密码"></title-info>
     <div class="content">
-      <mt-field label="手机号码:" placeholder="请输入手机号码" style="color:red;" class="horizontal"></mt-field>
+      <mt-field label="手机号码:" v-model="phone" placeholder="请输入手机号码" style="color:red;" class="horizontal" :attr="{maxlength: 11 }"></mt-field>
       <div style="display: flex;flex-direction: row;">
-        <mt-field label="验 证 码" placeholder="请输入验证码" style="color:red;flex:3;" class="horizontal" v-model="verifyCode"></mt-field>
+        <mt-field label="验 证 码" v-model="verifyCode" placeholder="请输入验证码" style="color:red;flex:3;" class="horizontal" :attr="{maxlength: 4 }"></mt-field>
         <img :src="fullPathImg" style="flex: 1;" width="120" height="49" @click.navite="changeImagesVerifyCode"/>
       </div>
-      <mt-field label="手机验证码:" placeholder="请输入验证码" style="color:red;" class="horizontal">
+      <mt-field label="手机验证码:"  v-model="msgCode" placeholder="请输入短信验证码" style="color:red;" class="horizontal">
         <mt-cell title="获取验证码" to="www.baidu.com" class="authcode"></mt-cell>
       </mt-field>
-      <mt-field label="请输入新密码:" type="password" placeholder="请输入最少6位数密码" style="color:red;" class="horizontal"></mt-field>
-      <mt-field label="请确认新密码:" type="password" placeholder="请输入最少6位数密码" style="color:red;" class="horizontal"></mt-field>
-      <mt-button type="primary" size="large" class="registerButton">重设密码</mt-button>
+      <mt-field label="请输入新密码:" v-model="password" type="password" placeholder="请输入最少6位数密码" style="color:red;" class="horizontal" :attr="{maxlength: 18 }"></mt-field>
+      <mt-field label="请确认新密码:" v-model="validPwd" type="password" placeholder="请输入最少6位数密码" style="color:red;" class="horizontal" :attr="{maxlength: 18 }"></mt-field>
+      <mt-button type="primary" size="large" class="registerButton" @click.navite="updatePwd">重设密码</mt-button>
     </div>
   </div>
 </template>
@@ -25,6 +25,7 @@
   import 'mint-ui/lib/style.css'
   import TitleInfo from '../common/TitleInfo'
   import {urlPrefix} from '../../common/const'
+  import {validatePhone, validateStrLength} from '../../common/FormValidate'
 
   Vue.use(Mint)
   export default {
@@ -36,7 +37,11 @@
     data: function () {
       return {
         verifyCode: '',
-        fullPathImg: ''
+        fullPathImg: '',
+        phone: '13242871762',
+        password: '111111',
+        validPwd: '',
+        msgCode: ''
       }
     },
     mounted: function () {
@@ -48,6 +53,37 @@
         this.$http.post('/validate/createImagesVerifyCode').then((response) => {
           this.fullPathImg = urlPrefix + response.data
           this.verifyCode = ''
+        })
+      },
+      updatePwdBefore: function () {
+        var flag = true
+        var errorMsg = ''
+        if (!validatePhone(this.phone)) {
+          flag = false
+          errorMsg = '手机号码'
+        } else if (this.verifyCode === null || this.verifyCode.trim() === '') {
+          flag = false
+          errorMsg = '验证码'
+        } else if (this.msgCode === null || this.msgCode.trim() === '') {
+          flag = false
+          errorMsg = '短信验证码'
+        } else if (!validateStrLength(this.password)) {
+          flag = false
+          errorMsg = '密码'
+        } else if (this.validPwd !== this.password) {
+          flag = false
+          errorMsg = '确认密码'
+        }
+        if (!flag) {
+          this.$toast(errorMsg + '输入格式有误!')
+        }
+        return flag
+      },
+      updatePwd: function () {
+        if (!this.updatePwdBefore()) {
+          return false
+        }
+        this.$http.post('').then((response) => {
         })
       }
     }
