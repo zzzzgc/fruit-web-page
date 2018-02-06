20aabe5cb5e5399efa02adc510abb1328be839b0
partnerccz <2779017796@qq.com>
2018-02-06 17:12:21 +0800

    修改忘记密码表单提交验证码的校验

--- src/components/login/ForgetPwd.vue
+++ src/components/login/ForgetPwd.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="content">
         <mt-field label="手机号码"  v-model="phone" placeholder="请输入手机号码" :attr="{ maxlength:11}" style="color:red;" class="horizontal"></mt-field>
-        <mt-field label="验 证 码" placeholder="请输入验证码" style="color:red;" class="horizontal">
+        <mt-field label="验 证 码" v-model="validCode" placeholder="请输入验证码" style="color:red;" class="horizontal">
           <mt-cell title="获取验证码" to="www.baidu.com" class="authcode"></mt-cell>
         </mt-field>
         <mt-field label="新 密 码" v-model="password" type="password"  placeholder="请输入最少6位数密码" :attr="{maxlength: 18 }"  style="color:red;" class="horizontal"></mt-field>
@@ -45,6 +45,9 @@
         if (!validatePhone(this.phone)) {
           flag = false
           errorMsg = '手机号码'
+        } else if (this.validCode === null || this.validCode.trim() === '') {
+          flag = false
+          errorMsg = '验证码'
         } else if (!validateStrLength(this.password)) {
           flag = false
           errorMsg = '新密码'
