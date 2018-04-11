1ba24cc635dd5919db6a56041d3db270c25bb7ea
partnerccz <2779017796@qq.com>
2018-04-11 22:03:23 +0800

    修改IP访问路径

--- src/common/const.js
+++ src/common/const.js
@@ -4,9 +4,15 @@ export const localStorageKeys = {
   typePageSize: 'type-page-size',
   productPageSize: 'product-page-size'
 }
-export const ajaxPrefix = '/'// ajax请求url前缀，测试环境下使用/api开头，前端做代理（url前缀匹配），生产环境为/ent
+export const ajaxPrefix = '/api'// ajax请求url前缀，测试环境下使用/api开头，前端做代理（url前缀匹配），生产环境为/ent
+// export const ajaxPrefix = '/'// ajax请求url前缀，测试环境下使用/api开头，前端做代理（url前缀匹配），生产环境为/ent
 
 export const homeUrl = '/' // 登录后跳转页面
 
-export const urlPrefix = 'http://localhost:8090/'
-export const imgUrlPrefix = 'http://localhost:8090/static/images/'
+// export const urlPrefix = 'http://localhost:8090/'
+// export const imgUrlPrefix = 'http://localhost:8090/static/images/'
+// export const imgUrlPrefix2 = 'http://localhost:8090/static/img/'
+
+export const urlPrefix = 'http://192.168.3.123:8090/'
+export const imgUrlPrefix = 'http://192.168.3.123:8090/static/images/'
+export const imgUrlPrefix2 = 'http://192.168.3.123:8090/static/img/'
