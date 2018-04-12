7c364fcb43313b43300a9e05d3a88b02b18addb0
zzzzgc <1098766713@qq.com>
2018-04-12 15:16:03 +0800

    提交测试环境代码

--- src/common/const.js
+++ src/common/const.js
@@ -9,10 +9,10 @@ export const ajaxPrefix = '/api'// ajax请求url前缀，测试环境下使用/a
 
 export const homeUrl = '/' // 登录后跳转页面
 
-// export const urlPrefix = 'http://localhost:8090/'
-// export const imgUrlPrefix = 'http://localhost:8090/static/images/'
-// export const imgUrlPrefix2 = 'http://localhost:8090/static/img/'
-
-export const urlPrefix = 'http://192.168.3.123:8090/'
-export const imgUrlPrefix = 'http://192.168.3.123:8090/static/images/'
-export const imgUrlPrefix2 = 'http://192.168.3.123:8090/static/img/'
+export const urlPrefix = 'http://localhost:8090/'
+export const imgUrlPrefix = 'http://localhost:8090/static/images/'
+export const imgUrlPrefix2 = 'http://localhost:8090/static/img/'
+//
+// export const urlPrefix = 'http://192.168.3.123:8090/'
+// export const imgUrlPrefix = 'http://192.168.3.123:8090/static/images/'
+// export const imgUrlPrefix2 = 'http://192.168.3.123:8090/static/img/'
