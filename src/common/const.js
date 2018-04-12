c33d9aed1cb34316634a1aecd660cefc6f3f838e
zzzzgc <1098766713@qq.com>
2018-04-12 16:37:02 +0800

    提交测试环境代码

--- src/common/const.js
+++ src/common/const.js
@@ -8,13 +8,13 @@ export const localStorageKeys = {
 export const homeUrl = '/' // 登录后跳转页面
 
 /* 本地: 上传路径 */
-// export const ajaxPrefix = '/api'// ajax请求url前缀，测试环境下使用/api开头，前端做代理（url前缀匹配），生产环境为/ent
-// export const urlPrefix = 'http://localhost:8090/'
-// export const imgUrlPrefix = 'http://localhost:8090/static/images/'
-// export const imgUrlPrefix2 = 'http://localhost:8090/static/img/'
+export const ajaxPrefix = '/api'// ajax请求url前缀，测试环境下使用/api开头，前端做代理（url前缀匹配），生产环境为/ent
+export const urlPrefix = 'http://localhost:8090/'
+export const imgUrlPrefix = 'http://localhost:8090/static/images/'
+export const imgUrlPrefix2 = 'http://localhost:8090/static/img/'
 
 /* 测试: 上传路径 */
-export const ajaxPrefix = '/'// ajax请求url前缀，测试环境下使用/api开头，前端做代理（url前缀匹配），生产环境为/ent
-export const urlPrefix = 'http://192.168.3.123:8090/'
-export const imgUrlPrefix = 'http://192.168.3.123:8090/static/images/'
-export const imgUrlPrefix2 = 'http://192.168.3.123:8090/static/img/'
+// export const ajaxPrefix = '/'// ajax请求url前缀，测试环境下使用/api开头，前端做代理（url前缀匹配），生产环境为/ent
+// export const urlPrefix = 'http://192.168.3.123:8090/'
+// export const imgUrlPrefix = 'http://192.168.3.123:8090/static/images/'
+// export const imgUrlPrefix2 = 'http://192.168.3.123:8090/static/img/'
