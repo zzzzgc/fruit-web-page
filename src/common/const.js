b9c3c819cfd1e0652b22fa6b2bf5aa690b766bef
zzzzgc <1098766713@qq.com>
2018-04-28 15:37:52 +0800

    修改正式服路径

--- src/common/const.js
+++ src/common/const.js
@@ -25,8 +25,8 @@ export const downloadFilePath = 'http://localhost:8080/upload/file/'
 
 /* 正式: 上传路径 */
 // export const ajaxPrefix = '/'// ajax请求url前缀，测试环境下使用/api开头，前端做代理（url前缀匹配），生产环境为/ent
-// export const urlPrefix = 'http://120.79.173.122:8090/'
-// export const imgUrlPrefix = 'http://120.79.173.122:8090/static/images/'
-// export const imgUrlPrefix2 = 'http://120.79.173.122:8090/upload/file/'
-// export const uploadFilePath = 'http://120.79.173.122:8080/manage/common/upload'
-// export const downloadFilePath = 'http://120.79.173.122:8080/upload/file/'
+// export const urlPrefix = 'http://b.52xiguo.com/'
+// export const imgUrlPrefix = 'http://b.52xiguo.com/static/images/'
+// export const imgUrlPrefix2 = 'http://b.52xiguo.com/upload/file/'
+// export const uploadFilePath = 'http://admin.52xiguo.com/manage/common/upload'
+// export const downloadFilePath = 'http://admin.52xiguo.com/upload/file/'
